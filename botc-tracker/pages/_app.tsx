5d17c8160f124b72291cdac81f69dbf1cedebe88
Ellie <101657425+elliehl@users.noreply.github.com>
2023-01-07 22:58:10 +0000

    Update font and add further styling

--- botc-tracker/pages/_app.tsx
+++ botc-tracker/pages/_app.tsx
@@ -1,15 +1,20 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import GameData from './GameData'
+import NavBar from '../components/NavBar'
+import { Abel } from '@next/font/google'
+
+const abel = Abel({ weight: '400', subsets: ['latin'] })
 
 export default function App() {
   return (
-  <div>
+  <div className={abel.className}>
     <Head>
         <title>BOTC Tracker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-  <GameData />
+     <NavBar />
+     <GameData />
   </div>
   )
 }
