df273eeaf2e3cbd412492cdd98fb345d6a944ae1
Ellie <101657425+elliehl@users.noreply.github.com>
2023-01-08 17:34:06 +0000

    Remove app file from index

--- botc-tracker/pages/index.tsx
+++ botc-tracker/pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         <title>BOTC Tracker</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <App />
     </div>
   )
 }
