5377234fd1a95869c704ce94022f2626e9821bf9
Ellie <101657425+elliehl@users.noreply.github.com>
2023-01-08 17:33:48 +0000

    Update styling

--- botc-tracker/components/NavBar.tsx
+++ botc-tracker/components/NavBar.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from '../styles/NavBar.module.css'
+import Link from "next/link";
 
 const NavBar = () => {
     return (
         <div className={styles["navbar-container"]}>
             <div className={styles["navbar-left"]}>
-                <h4>Player Stats</h4>
+                <Link href="/GameData" className={styles["page-links"]}>Player Stats</Link>
                 <h4>Storyteller Stats</h4>
             </div>
             <div className={styles["navbar-right"]}>
-                <h4>About</h4>
+                <Link href="/about" className={styles["page-links"]}>About</Link>
                 <h4>Contact</h4>
             </div>
         </div>
