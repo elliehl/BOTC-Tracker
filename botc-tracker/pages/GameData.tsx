cd5309324b8753f4c9470c2c86d2d60cbafe5d81
Ellie <101657425+elliehl@users.noreply.github.com>
2023-01-09 18:42:12 +0000

    Add test data and display most played role

--- botc-tracker/pages/GameData.tsx
+++ botc-tracker/pages/GameData.tsx
@@ -10,6 +10,15 @@ interface Game {
     date?: Date
 }
 
+interface Characters {
+    readonly id: number
+    name: string
+    isGoodAligned: boolean
+    gamesPlayed: number
+    totalWins: number
+    winPercentage: number
+}
+
 // TEST DATA
 const gameRecords: Game[] = [
     {
@@ -52,21 +61,97 @@ const gameRecords: Game[] = [
     isGoodAligned: true,
     date: new Date('2022-12-27')
     },
-
 ]
 
+const characters: Characters[] = [
+    {
+     id: 1,
+     name: 'Artist',
+     isGoodAligned: true,
+     gamesPlayed: 10,
+     totalWins: 6,
+     winPercentage: 60   
+    },
+    {
+     id: 2,
+     name: 'Clockmaker',
+     isGoodAligned: true,
+     gamesPlayed: 20,
+     totalWins: 10,
+     winPercentage: 50   
+    },
+    {
+     id: 3,
+     name: 'Witch',
+     isGoodAligned: false,
+     gamesPlayed: 5,
+     totalWins: 4,
+     winPercentage: 80   
+    },
+    {
+     id: 4,
+     name: 'Vortox',
+     isGoodAligned: false,
+     gamesPlayed: 8,
+     totalWins: 2,
+     winPercentage: 25   
+    },
+    {
+     id: 5,
+     name: 'Klutz',
+     isGoodAligned: true,
+     gamesPlayed: 2,
+     totalWins: 0,
+     winPercentage: 0   
+    },
+    {
+     id: 6,
+     name: 'Philosopher',
+     isGoodAligned: true,
+     gamesPlayed: 11,
+     totalWins: 11,
+     winPercentage: 100   
+    },
+]
+
+function findMostGamesPlayed (arr: Characters[]) {
+    let top = {
+        id: -1,
+        name: 'Test1',
+        isGoodAligned: true,
+        gamesPlayed: -Infinity,
+        totalWins: -Infinity,
+        winPercentage: -Infinity   
+       }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].gamesPlayed > top.gamesPlayed) {
+            top = arr[i]
+        } 
+    }
+    return top
+}
+
+
 const GameData = () => {
+    let topData = findMostGamesPlayed(characters)
+
     return (
         <>
-        <div className={styles['role-stats-container']}>
+        <div className={styles['game-history-container']}>
         {gameRecords.map((game) => {
            return (
-            <div className={styles['role-list-item']}>
+            <div className={styles['history-list-item']}>
            <h3 key={game.id} >{game.finalRole}</h3>
            <h3 key={game.id}>{game.isOnWinningTeam ? 'Win' : 'Loss'}</h3>
            </div>
            )
           })}
+        </div>
+        <div className={styles['role-stats-container']}>
+          <h3>{topData.name}</h3>
+          <h3>{topData.gamesPlayed}</h3>
+          <h3>{topData.winPercentage + '%'}</h3>
         </div>
         </>
     )
