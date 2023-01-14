2090aded6dbc4e90a1a5f6253c1603ced7d3f6f6
Ellie <101657425+elliehl@users.noreply.github.com>
2023-01-14 21:20:09 +0000

    Add top five most played role tracking functionality

--- botc-tracker/pages/GameData.tsx
+++ botc-tracker/pages/GameData.tsx
@@ -19,6 +19,25 @@ interface Characters {
     winPercentage: number
 }
 
+interface CharacterTypes {
+    readonly id: number
+    name: string
+    isGoodAligned: boolean
+    gamesPlayed: number
+    totalWins: number
+    winPercentage: number
+}
+
+interface Alignment {
+    readonly id: number
+    gamesPlayedGood: number
+    gamesWonGood: number
+    winPercentageGood: number
+    gamesPlayedEvil: number
+    gamesWonEvil: number
+    winPercentageEvil: number
+}
+
 // TEST DATA
 const gameRecords: Game[] = [
     {
@@ -100,7 +119,7 @@ const characters: Characters[] = [
      id: 5,
      name: 'Klutz',
      isGoodAligned: true,
-     gamesPlayed: 2,
+     gamesPlayed: 9,
      totalWins: 0,
      winPercentage: 0   
     },
@@ -114,27 +133,57 @@ const characters: Characters[] = [
     },
 ]
 
-function findMostGamesPlayed (arr: Characters[]) {
-    let top = {
-        id: -1,
-        name: 'Test1',
-        isGoodAligned: true,
-        gamesPlayed: -Infinity,
-        totalWins: -Infinity,
-        winPercentage: -Infinity   
-       }
+const characterTypes: CharacterTypes[] = [
+    {
+    id: 1,
+    name: 'Townsfolk',
+    isGoodAligned: true,
+    gamesPlayed: 78,
+    totalWins: 39,
+    winPercentage: 50   
+    },
+    {
+    id: 2,
+    name: 'Outsider',
+    isGoodAligned: true,
+    gamesPlayed: 15,
+    totalWins: 9,
+    winPercentage: 60   
+    },
+    {
+    id: 3,
+    name: 'Minion',
+    isGoodAligned: false,
+    gamesPlayed: 40,
+    totalWins: 30,
+    winPercentage: 75   
+    },
+    {
+    id: 4,
+    name: 'Demon',
+    isGoodAligned: false,
+    gamesPlayed: 12,
+    totalWins: 3,
+    winPercentage: 25   
+    },
+    {
+    id: 5,
+    name: 'Traveller',
+    isGoodAligned: true,
+    gamesPlayed: 1,
+    totalWins: 1,
+    winPercentage: 100   
+    },
+]
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].gamesPlayed > top.gamesPlayed) {
-            top = arr[i]
-        } 
-    }
-    return top
-}
-
+function findTopFiveMostGamesPlayed (arr: Characters[]) {
+    let sortRanking = characters.sort((a, b) => b.gamesPlayed - a.gamesPlayed)
+    let topFive = sortRanking.slice(0,5)
+    return topFive
+} 
 
 const GameData = () => {
-    let topData = findMostGamesPlayed(characters)
+    let topData = findTopFiveMostGamesPlayed(characters)
 
     return (
         <>
@@ -142,16 +191,23 @@ const GameData = () => {
         {gameRecords.map((game) => {
            return (
             <div className={styles['history-list-item']}>
-           <h3 key={game.id} >{game.finalRole}</h3>
-           <h3 key={game.id}>{game.isOnWinningTeam ? 'Win' : 'Loss'}</h3>
-           </div>
+                <h3 key={game.id} >{game.finalRole}</h3>
+                <h3 key={game.id}>{game.isOnWinningTeam ? 'Win' : 'Loss'}</h3>
+            </div>
            )
           })}
         </div>
         <div className={styles['role-stats-container']}>
-          <h3>{topData.name}</h3>
-          <h3>{topData.gamesPlayed}</h3>
-          <h3>{topData.winPercentage + '%'}</h3>
+            {topData.map((rank) => {
+                return (
+                    <div className="played-list">
+                        <h3>{rank.name}</h3>
+                        <h3>{rank.gamesPlayed}</h3>
+                        <h3>{rank.winPercentage + '%'}</h3>
+                    </div>
+                )
+            })
+            }
         </div>
         </>
     )
