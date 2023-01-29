b45ac1b4b511f4423c31ab182c7a0edcd3ddd4ce
Ellie <101657425+elliehl@users.noreply.github.com>
2023-01-29 19:59:13 +0000

    Change form buttons to checkboxes

--- botc-tracker/components/GameData/AddGameButton.tsx
+++ botc-tracker/components/GameData/AddGameButton.tsx
@@ -6,6 +6,7 @@ import roleDataArray from "./roleData";
 const roleList = roleDataArray.map((role) => role.name).sort()
 
 const AddGameButton = () => {
+
     const [viewModal, setViewModal] = useState(false)
 
     return (
@@ -13,8 +14,8 @@ const AddGameButton = () => {
         <div className={styles["add-game-button-container"]}>
         <button className={styles["add-game-button"]} onClick={() => setViewModal(true)}>Add Game</button>
         </div>
-        <Modal isOpen={viewModal} onRequestClose={() => setViewModal(false)}>
-            <form action="/GameData" method="post">
+        <Modal isOpen={viewModal} onRequestClose={() => setViewModal(false)} className={styles["modal"]}>
+            <form action="/GameData" method="post" className={styles["form"]}>
                 <div>
                     <label>Starting Role</label>
                     <input list="roles" name="Starting Role" required={true}></input>
@@ -31,23 +32,21 @@ const AddGameButton = () => {
                 </div>
                 <div className={styles["alignment-container"]}>
                     <label>Alignment</label>
-                    <div className={styles["alignment-button-container"]}>
-                        <input type="button" className={styles["choice-button-left"]} value="Good"></input>
-                        <input type="button" className={styles["choice-button-right"]} value="Evil"></input>
+                    <div className={styles["button-container"]}>
+                        <input type="checkbox" className={styles["alignment-button"]} value="Good"></input>
                     </div>
                 </div>
                 <div className={styles["result-container"]}>
                     <label>Result</label>
-                    <div className={styles["alignment-button-container"]}>
-                        <input type="button" className={styles["choice-button-left"]} value="Win"></input>
-                        <input type="button" className={styles["choice-button-right"]} value="Loss"></input>
+                    <div className={styles["button-container"]}>
+                        <input type="checkbox" className={styles["result-button"]} value="Win"></input>
                     </div>
                 </div>
                 <div>
                     <label>Date</label>
                     <input type="date"></input>
                 </div>
-                <div>
+                <div className="bottom-modal-buttons">
                     <button type="submit">Submit</button>
                     <button onClick={() => setViewModal(false)}>Cancel</button>
                 </div>
