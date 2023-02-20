3a4d46a9745a0323d4008215e448a5d693d4f750
Ellie <101657425+elliehl@users.noreply.github.com>
2023-02-20 12:21:34 +0000

    Update folders

---
+++ botc-tracker/client/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from '../styles/NavBar.module.css'
+import Link from "next/link";
+
+const NavBar = () => {
+    return (
+        <div className={styles["navbar-container"]}>
+            <div className={styles["navbar-left"]}>
+                <Link href="/GameData" className={styles["page-links"]}>Player Stats</Link>
+            </div>
+            <div className={styles["navbar-right"]}>
+                <Link href="/about" className={styles["page-links"]}>About</Link>
+                <Link href="/contact" className={styles["page-links"]}>Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ botc-tracker/client/pages/_app.tsx
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+import Head from 'next/head'
+import NavBar from '../components/NavBar'
+import { Abel } from '@next/font/google'
+import type { AppProps } from 'next/app'
+
+const abel = Abel({ weight: '400', subsets: ['latin'] })
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+  <div className={abel.className}>
+    <Head>
+        <title>BOTC Tracker</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+     <NavBar />
+     <Component {...pageProps} />
+  </div>
+  )
+}
