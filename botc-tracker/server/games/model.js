15f84f78640f754c3a4dd69a435f7f724986262e
Ellie <101657425+elliehl@users.noreply.github.com>
2023-05-19 01:19:30 +0100

    Remove console logs

--- botc-tracker/server/games/model.js
+++ botc-tracker/server/games/model.js
@@ -4,10 +4,8 @@ const fetchGames1 = async () => {
   try {
     const connection = await db.getConnection();
     const [rows] = await connection.query(`SELECT * FROM games`);
-    console.log(rows, "rows");
     return rows;
   } catch (err) {
-    console.error(err);
     throw err;
   }
 };
