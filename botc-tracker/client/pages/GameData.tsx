cf76539fc1b4e9d9fbb121fbc11d9ff394007d20
Ellie <101657425+elliehl@users.noreply.github.com>
2023-05-31 12:13:20 +0100

    Change component name

--- botc-tracker/client/pages/GameData.tsx
+++ botc-tracker/client/pages/GameData.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import styles from '../styles/gameData.module.css'
 import AlignmentStats from "../components/GameData/AlignmentStats"
 import CharacterTypeStats from "../components/GameData/CharacterTypeStats"
-import GameHistory from "../components/GameData/GameHistory"
+import DisplayGames from "../components/GameData/GameHistory"
 import RoleStats from "../components/GameData/RoleStats"
 import "../styles/AlignmentStats.module.css"
 import AddGameButton from "../components/GameData/AddGameButton"
@@ -15,7 +15,7 @@ const GameData = () => {
         <div className={styles["main-page"]}>
             <AlignmentStats />
             {/* <CharacterTypeStats /> */}
-            <GameHistory />
+            <DisplayGames />
             <RoleStats />
         </div>
         </>
