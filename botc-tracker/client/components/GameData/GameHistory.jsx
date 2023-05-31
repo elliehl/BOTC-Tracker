ab26f5fe2fa46a46b8c8dbf3f437ce94f374b9c9
Ellie <101657425+elliehl@users.noreply.github.com>
2023-05-31 15:39:22 +0100

    Add more headings

--- botc-tracker/client/components/GameData/GameHistory.jsx
+++ botc-tracker/client/components/GameData/GameHistory.jsx
@@ -17,9 +17,11 @@ const DisplayGames = () => {
         <div className={styles['game-history-container']}>
         <>
         {gameHistory.map((game) => {
-            return (<div key={game.id}>
+            return (<div key={game.id} className={styles['history-list-item']}>
+                <h3>{game.starting_role}</h3>
+                <h3>{game.is_evil === 0 ? 'Good' : 'Evil'}</h3>
+                <h3>{game.game_won === 0 ? 'Loss' : 'Win'}</h3>
                 <h3>{game.comments}</h3>
-                <h3>{game.starting_role_id}</h3>
             </div>
             )
         })
