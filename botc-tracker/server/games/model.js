cb62d7755c267f13534f8e96b42ba04c0d21d05d
Ellie <101657425+elliehl@users.noreply.github.com>
2023-06-03 17:45:12 +0100

    Update query to add final role

--- botc-tracker/server/games/model.js
+++ botc-tracker/server/games/model.js
@@ -4,9 +4,10 @@ const fetchGames1 = async () => {
   try {
     const connection = await db.getConnection();
     const [gameData] = await connection.query(
-      `SELECT date, game_won, is_evil, comments, r.name AS starting_role, t.name as type
+      `SELECT date, game_won, is_evil, comments, r.name AS starting_role, r1.name as final_role, t.name as type
           FROM games g 
           JOIN roles r ON r.id = g.starting_role_id
+          JOIN roles r1 ON r1.id = g.final_role_id
           JOIN types t ON t.id = r.type_id;`
     );
     return gameData;
