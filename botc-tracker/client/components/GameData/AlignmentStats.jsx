d994924966476bfef4ceae1d021025d3d0a51730
Ellie <101657425+elliehl@users.noreply.github.com>
2023-06-05 16:20:03 +0100

    Add colon for cleaner look

--- botc-tracker/client/components/GameData/AlignmentStats.jsx
+++ botc-tracker/client/components/GameData/AlignmentStats.jsx
@@ -19,7 +19,7 @@ const AlignmentStats = () => {
         {alignmentStats.map((stats) => {
             return (<div key={stats.id} className={styles['alignment-list']}>
                 <>
-                <h3>{stats.is_evil === 0 ? 'Good Wins' : 'Evil Wins'} {stats.wins}</h3>
+                <h3>{stats.is_evil === 0 ? 'Good Wins: ' : 'Evil Wins: '} {stats.wins}</h3>
                 </>
             </div>
             )
