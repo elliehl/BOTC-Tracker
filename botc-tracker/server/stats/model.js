947210f440cc5933429abfd917e28fb4cfcbc4f9
Ellie <101657425+elliehl@users.noreply.github.com>
2023-06-06 16:26:00 +0100

    Add WHERE to show wins instead of games played

--- botc-tracker/server/stats/model.js
+++ botc-tracker/server/stats/model.js
@@ -6,6 +6,7 @@ const fetchStatsByAlignment = async () => {
     const [alignmentData] = await connection.query(
       `SELECT is_evil, COUNT(game_won) AS wins
        FROM games g
+       WHERE game_won = 1
        GROUP BY is_evil;`
     );
     return alignmentData;
