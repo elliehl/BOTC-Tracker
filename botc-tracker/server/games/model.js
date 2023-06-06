815b477e16e5a81cfac30c328d1010ab72780410
Ellie <101657425+elliehl@users.noreply.github.com>
2023-06-06 15:29:44 +0100

    Add id to fetchGames query and add deleteGame

--- botc-tracker/server/games/model.js
+++ botc-tracker/server/games/model.js
@@ -4,7 +4,7 @@ const fetchGames1 = async () => {
   try {
     const connection = await db.getConnection();
     const [gameData] = await connection.query(
-      `SELECT date, game_won, is_evil, comments, r.name AS starting_role, r1.name as final_role, t.name as type
+      `SELECT date, game_won, is_evil, comments, r.name AS starting_role, r1.name as final_role, t.name as type, g.id
           FROM games g 
           JOIN roles r ON r.id = g.starting_role_id
           JOIN roles r1 ON r1.id = g.final_role_id
@@ -16,4 +16,24 @@ const fetchGames1 = async () => {
   }
 };
 
-module.exports = { fetchGames1 };
+const deleteGame = async (id) => {
+  try {
+    const connection = await db.getConnection();
+    return connection.query(`DELETE FROM games WHERE id = ${id}`);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// const addGame = async () => {
+//   try {
+//     const connection = await db.getConnection();
+//     let date = gameData.date === "" ? null : `'${gameData.date}'`;
+//     return connection.query(`INSERT INTO games(date, game_won, is_evil, comments, starting_role_id, final_role_id)
+//     VALUES (${date}, ${gameData.result}, ${gameData.alignment}, '${gameData.comments}', ${gameData.startingRole}, ${gameData.finalRole});`);
+//   } catch (err) {
+//     throw err;
+//   }
+// };
+
+module.exports = { fetchGames1, deleteGame };
