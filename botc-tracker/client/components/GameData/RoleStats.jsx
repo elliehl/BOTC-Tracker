39698bb321038c568e6048bd811bb87b4b85a137
Ellie <101657425+elliehl@users.noreply.github.com>
2023-06-13 13:06:33 +0100

    Update with real database data

---
+++ botc-tracker/client/components/GameData/RoleStats.jsx
@@ -0,0 +1,48 @@
+import {React, useState, useEffect} from "react";
+import styles from "../../styles/RoleStats.module.css"
+
+const RoleStats = () => {
+    const [roleStats, setRoleStats] = useState([])
+
+    const getRoleStats = async () => {
+        const res = await fetch('http://localhost:9090/roles')
+        res.json().then((res) => setRoleStats(res.stats))
+    }
+
+    useEffect(() => {
+        getRoleStats()
+    }, [])
+
+    const findTopFiveMostGamesPlayed = () => {
+        let sortRanking = roleStats.sort((a, b) => b.games - a.games)
+        console.log(sortRanking, 'sort ranking')
+        let topFive = sortRanking.slice(0,5)
+        return topFive
+    } 
+
+    let topData = findTopFiveMostGamesPlayed(roleStats)
+
+    return (
+        <div className={styles['role-stats-container']}>
+            <div className="role-stats-titles">
+            <h3>Role</h3>
+            <h3>Games</h3>
+            <h3>Wins</h3>
+            <h3>Win %</h3>
+            </div>
+            {topData.map((rank) => {
+                return (
+                    <div className="played-list">
+                        <h3>{rank.starting_role}</h3>
+                        <h3>{rank.games}</h3>
+                        <h3>{rank.wins}</h3>
+                        <h3>{Math.round((rank.wins / rank.games) * 100)}%</h3>
+                    </div>
+                )
+            })
+            }
+        </div>
+    )
+}
+
+export default RoleStats
