f185240d32e56a46761a37a0537ef1529d1bdde7
Ellie <101657425+elliehl@users.noreply.github.com>
2023-08-23 12:19:41 +0100

    Minor text update

--- botc-tracker/client/pages/about.tsx
+++ botc-tracker/client/pages/about.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return (
         <div>
-        <p>This page currently only supports official roles from the game.</p>
+        <p>This tracker only supports official roles from the game.</p>
         <p>Lil' Monsta isn't included as a playable character since the starting role is a minion, so with the way I decided to display the data it would be impossible to play as Lil' Monsta.</p>
         </div>
     )
