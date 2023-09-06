94c7af65213fd7a266bb46a4b5ebf51a14a1519f
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-06 14:08:46 +0100

    Change to link to .NET backend

--- botc-tracker/client/components/GameData/GameHistory.jsx
+++ botc-tracker/client/components/GameData/GameHistory.jsx
@@ -3,17 +3,18 @@ import styles1 from "../../styles/GameHistory.module.css"
 import styles from "../../styles/AddGameButton.module.css"
 const Modal = require('react-modal')
 import roleDataArray from "./roleData";
+import { createTrue } from "typescript";
 const roleList = roleDataArray.map((role) => role.name).sort()
 const roleToSend = roleDataArray.map((role) => ({roleName: role.name, id: role.id}))
 
 const DisplayGames = () => {
     const [gameHistory, setGameHistory] = useState([])
-    const [selectedGame, setSelectedGame] = useState({alignment: false, result: false, startingRole: '', finalRole: '', date: '', comments: ''})
+    // const [selectedGame, setSelectedGame] = useState({alignment: false, result: false, startingRole: '', finalRole: '', date: '', comments: ''})
 
     const getGames = async () => {
         try {
-        const res = await fetch('http://localhost:9090/games')
-        res.json().then((res) => setGameHistory(res.games))
+        const res = await fetch('https://localhost:7240/api/Games')
+        res.json().then((res) => setGameHistory(res))
         } catch(err) {
             console.log(err)
             throw err
@@ -22,6 +23,7 @@ const DisplayGames = () => {
 
     const AddGameButton = () => {
     
+    const [id, setId] = useState(null)
     const [viewModal, setViewModal] = useState(false)
     const [alignment, setAlignment] = useState(false)
     const [result, setResult] = useState(false)
@@ -33,25 +35,24 @@ const DisplayGames = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            let res = await fetch('http://localhost:9090/', {
+            let res = await fetch('https://localhost:7240/api/Games', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    alignment: alignment,
-                    result: result,
-                    startingRole: (roleToSend.filter((obj) => startingRole === obj.roleName))[0].id,
-                    finalRole: (roleToSend.filter((obj) => finalRole === obj.roleName))[0].id,
-                    date: date,
-                    comments: comments
+                    Is_Evil: alignment,
+                    Game_Won: result,
+                    Starting_Role: startingRole,
+                    Final_Role: finalRole,
+                    Date: date,
+                    Comments: comments
                 })
             });
             let jsonResponse = await res.json()
-            // the 2 lines below are needed to actually show the roles for the node backend, otherwise it shows as blank because
-            // it's initially being given the id number not the role string
-            jsonResponse.starting_role = startingRole
-            jsonResponse.final_role = finalRole
+
+            // jsonResponse.starting_role = startingRole
+            // jsonResponse.final_role = finalRole
             if (res.status === 201) {
                 console.log(jsonResponse)
                 setGameHistory([...gameHistory, jsonResponse])
@@ -61,7 +62,9 @@ const DisplayGames = () => {
                 setFinalRole('')
                 setDate('')
                 setComments('')
+                setId(null)
             } else {
+                console.log(jsonResponse)
                 console.log('Response not OK')
             }
         } catch (err) {
@@ -89,10 +92,9 @@ const DisplayGames = () => {
                     </datalist>
                 </div>
                 <div className={styles["alignment-container"]}>
-                    <label htmlFor="alignment-box">Alignment</label>
-                    <div className={styles["button-container"]}>
-                        <input id="alignment-box" type="checkbox" className={styles["alignment-button"]} checked={alignment} onChange={e => setAlignment(!alignment)}></input>
-                    </div>
+                    <input id="alignment-box" type="checkbox" className={styles["alignment-toggle"]} checked={alignment} onChange={e => setAlignment(!alignment)}></input>
+                    <label className="good-aligned" htmlFor="alignment-box">Good</label>
+                    <label className="evil-aligned" htmlFor="alignment-box">Evil</label>
                 </div>
                 <div className={styles["result-container"]}>
                     <label htmlFor="result-box">Result</label>
@@ -119,7 +121,7 @@ const DisplayGames = () => {
 }
 
     const handleDelete = async (id) => {
-        await fetch(`http://localhost:9090/games/${id}`, {
+        await fetch(`https://localhost:7240/${id}`, {
             method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json'
@@ -135,21 +137,91 @@ const DisplayGames = () => {
         }).catch((err) => console.log(err))
     }
 
-    const handleUpdate = async (id) => {
-        await fetch(`http://localhost:9090/games/${id}`, {
-            method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                }, body: JSON.stringify({
-                    alignment: selectedGame.alignment,
-                    result: selectedGame.result,
-                    startingRole: (roleToSend.filter((obj) => selectedGame.startingRole === obj.roleName))[0].id,
-                    finalRole: (roleToSend.filter((obj) => selectedGame.finalRole === obj.roleName))[0].id,
-                    date: selectedGame.date,
-                    comments: selectedGame.comments
+
+       const HandleUpdate = () => {
+        const [viewEditModal, setViewEditModal] = useState(false)
+        const [alignment, setAlignment] = useState(false)
+        const [result, setResult] = useState(false)
+        const [startingRole, setStartingRole] = useState('')
+        const [finalRole, setFinalRole] = useState('')
+        const [date, setDate] = useState('')
+        const [comments, setComments] = useState('')
+
+        const editData = async (id) => {    
+            e.preventDefault()
+            try {
+            await fetch(`https://localhost:9090/${id}`, {
+                method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }, body: JSON.stringify({
+                        alignment: alignment,
+                        result: result,
+                        startingRole: (roleToSend.filter((obj) => startingRole === obj.roleName))[0].id,
+                        finalRole: (roleToSend.filter((obj) => finalRole === obj.roleName))[0].id,
+                        date: date,
+                        comments: comments
+                    })
                 })
-            })
-    } 
+            } catch (err) {
+                console.log('error', err)
+            }
+        }
+
+        return (
+            <>
+            <div className={styles["add-game-button-container"]}>
+            <button onClick={() => setViewEditModal(true)}>Edit</button>
+            </div>
+            <Modal isOpen={viewEditModal} onRequestClose={() => setViewEditModal(false)} className={styles["modal"]}>
+                <form action="/GameData" method="POST" id="overallForm" className={styles["form"]} onSubmit={editData}>
+                    <div>
+                        <label htmlFor="starting-role-box">Starting Role</label>
+                        <input id="starting-role-box" list="roles" name="Starting Role" required={true} value={startingRole} onChange={(e) => setStartingRole(e.target.value)}></input>
+                        <datalist id="roles">
+                            {roleList.map((roleName) => <option>{roleName}</option>)}
+                        </datalist>
+                    </div>
+                    <div>
+                        <label htmlFor="final-role-box">Final Role</label>
+                        <input id="final-role-box" list="roles" name="Final Role" required={true} value={finalRole} onChange={(e) => setFinalRole(e.target.value)}></input>
+                        <datalist id="roles">
+                            {roleList.map((roleName) => <option>{roleName}</option>)}
+                        </datalist>
+                    </div>
+                    <div className={styles["alignment-container"]}>
+                        <label htmlFor="alignment-box">Alignment</label>
+                        <div className={styles["button-container"]}>
+                            <input id="alignment-box" type="checkbox" className={styles["alignment-button"]} checked={alignment} onChange={e => setAlignment(!alignment)}></input>
+                        </div>
+                    </div>
+                    <div className={styles["result-container"]}>
+                        <label htmlFor="result-box">Result</label>
+                        <div className={styles["button-container"]}>
+                            <input id="result-box" type="checkbox" className={styles["result-button"]} checked={result} onChange={e => setResult(!result)}></input>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="date-box">Date</label>
+                        <input id="date-box" type="date" value={date} onChange={e => setDate(e.target.value)}></input>
+                    </div>
+                    <div>
+                        <label htmlFor="comments-box">Comments</label>
+                        <input id="comments-box" type="text" value={comments} onChange={e => setComments(e.target.value)}></input>
+                    </div>
+                    <div className="bottom-modal-buttons">
+                        <button type="submit">Submit</button>
+                        <button onClick={() => setViewEditModal(false)}>Cancel</button>
+                    </div>
+                </form>
+            </Modal>
+            </>
+        ) 
+       }
+        
+
+            
+            
 
     useEffect(() => {
         getGames()
@@ -159,6 +231,7 @@ const DisplayGames = () => {
         <div className={styles1['game-history-container']}>
         <>
         <AddGameButton/>
+        {console.log(gameHistory)}
         {gameHistory.length === 0 && 'No Game History'}
         <table>
             <thead>
@@ -175,16 +248,15 @@ const DisplayGames = () => {
             </thead>
             <tbody>
                 {gameHistory.map((game) => {
-                    console.log(game)
                     return (
                         <tr key={game.id} className={styles1['history-list-item']}>
-                            <td>{game.starting_role}</td>
-                            <td>{game.final_role}</td>
-                            <td>{game.is_evil === 0 || game.alignment === false ? 'Good' : 'Evil'}</td>
-                            <td>{game.game_won === 0 || game.result === false ? 'Loss' : 'Win'}</td>
+                            <td>{game.starting_Role}</td>
+                            <td>{game.final_Role}</td>
+                            <td>{game.is_Evil === false ? 'Good' : 'Evil'}</td>
+                            <td>{game.game_Won === false ? 'Loss' : 'Win'}</td>
                             <td>{game.comments}</td>
                             <td>{game.date === null ? 'No Date' : new Date(game.date).toDateString()}</td>
-                            <td><button onClick={() => handleUpdate(game.id)} type="button">Edit</button></td>
+                            <td><HandleUpdate/></td>
                             <td><button onClick={() => handleDelete(game.id)} type="button">Delete</button></td>
                         </tr>
                     )
