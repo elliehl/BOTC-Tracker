021166935ca9d1c864bdfafed51768676822df07
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-09 16:27:50 +0100

    Fix button and use correct function for delete

--- botc-tracker/client/components/GameData/Game.jsx
+++ botc-tracker/client/components/GameData/Game.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import { GameContext } from "../../contexts/GameContext"
-import Modal from "react-modal"
+// const Modal = require("react-modal")
 import "../../styles/AddGameButton.module.css"
 
 
@@ -23,12 +23,12 @@ const Game = ({game}) => {
             <td>{game.game_Won === false ? 'Loss' : 'Win'}</td>
             <td>{game.comments}</td>
             <td>{game.date === null ? 'No Date' : new Date(game.date).toDateString()}</td>
-            <td><button onClick={() => setViewEditModal(true)} type="button">Delete</button></td>
-            <td><button onClick={() => handleDelete(game.id)} type="button">Delete</button></td>
+            <td><button onClick={() => setViewEditModal(true)} type="button">Edit</button></td>
+            <td><button onClick={() => deleteGame(game.id)} type="button">Delete</button></td>
 
-            <Modal isOpen={viewEditModal} onRequestClose={() => setViewEditModal(false)} className="modal">
+            {/* <Modal isOpen={viewEditModal} onRequestClose={() => setViewEditModal(false)} className="modal">
                 <EditGameForm selectedGame={game}/>
-            </Modal>
+            </Modal> */}
         </>
     )
 }
