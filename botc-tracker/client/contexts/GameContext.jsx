b01a2b5b1f215e47d74b0213cc0eecc1c2a01c53
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-09 13:57:57 +0100

    Add initial GameContext with addGame

---
+++ botc-tracker/client/contexts/GameContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+
+export const GameContext = createContext();
+
+const GameContextProvider = (props) => {
+    const [gameHistory, setGameHistory] = useState([])
+
+    const addGame = async (e) => {
+        e.preventDefault()
+        try {
+            let res = await fetch('https://localhost:7240/api/Games', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    Is_Evil: alignment,
+                    Game_Won: result,
+                    Starting_Role: startingRole,
+                    Final_Role: finalRole,
+                    Date: date,
+                    Comments: comments
+                })
+            });
+            let jsonResponse = await res.json()
+
+            if (res.status === 201) {
+                console.log(jsonResponse)
+                setGameHistory([...gameHistory, jsonResponse])
+            } else {
+                console.log(jsonResponse)
+                console.log('Response not OK')
+            }
+        } catch (err) {
+            console.log('error', err)
+        }
+    }
+}
