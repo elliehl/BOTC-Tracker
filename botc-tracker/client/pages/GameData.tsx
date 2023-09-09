e1808e6a1720a4ef204dc179cc4a83d0d1123f5a
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-09 14:00:11 +0100

    Comment out non-functional components for now

--- botc-tracker/client/pages/GameData.tsx
+++ botc-tracker/client/pages/GameData.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import styles from '../styles/gameData.module.css'
-import AlignmentStats from "../components/GameData/AlignmentStats"
-import CharacterTypeStats from "../components/GameData/CharacterTypeStats"
+// import AlignmentStats from "../components/GameData/AlignmentStats"
+// import CharacterTypeStats from "../components/GameData/CharacterTypeStats"
 import DisplayGames from "../components/GameData/GameHistory"
-import RoleStats from "../components/GameData/RoleStats"
+// import RoleStats from "../components/GameData/RoleStats"
 import "../styles/AlignmentStats.module.css"
-import AddGameButton from "../components/GameData/AddGameButton"
-import RoleStatsTest from "../components/GameData/RoleStats"
+// import AddGameButton from "../components/GameData/AddGameButton"
+// import RoleStatsTest from "../components/GameData/RoleStats"
 
 const GameData = () => {
 
@@ -14,9 +14,9 @@ const GameData = () => {
         <>
         {/* <AddGameButton /> */}
         <div className={styles["main-page"]}>
-            <AlignmentStats />
-            <CharacterTypeStats />
-            <RoleStatsTest />
+            {/* <AlignmentStats /> */}
+            {/* <CharacterTypeStats /> */}
+            {/* <RoleStatsTest /> */}
         </div>
         <div className={styles["main-page"]}>
         <DisplayGames />
