ce4f3708e06499f6d4323d3ea0a61e48a8250a18
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-09 13:47:58 +0100

    Create separate component for AddGameForm

---
+++ botc-tracker/client/components/GameData/AddGameForm.jsx
@@ -0,0 +1,49 @@
+import styles from "../../styles/AddGameButton.module.css"
+
+
+const AddGameForm = () => {
+
+    return (
+        <form action="/GameData" method="POST" id="overallForm" className={styles["form"]} onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="starting-role-box">Starting Role</label>
+                <input id="starting-role-box" list="roles" name="Starting Role" required={true} value={startingRole} onChange={(e) => setStartingRole(e.target.value)}></input>
+                <datalist id="roles">
+                    {roleList.map((roleName) => <option>{roleName}</option>)}
+                </datalist>
+            </div>
+            <div>
+                <label htmlFor="final-role-box">Final Role</label>
+                <input id="final-role-box" list="roles" name="Final Role" required={true} value={finalRole} onChange={(e) => setFinalRole(e.target.value)}></input>
+                <datalist id="roles">
+                    {roleList.map((roleName) => <option>{roleName}</option>)}
+                </datalist>
+            </div>
+            <div className={styles["alignment-container"]}>
+                <input id="alignment-box" type="checkbox" className={styles["alignment-toggle"]} checked={alignment} onChange={e => setAlignment(!alignment)}></input>
+                <label className="good-aligned" htmlFor="alignment-box">Good</label>
+                <label className="evil-aligned" htmlFor="alignment-box">Evil</label>
+            </div>
+            <div className={styles["result-container"]}>
+                <label htmlFor="result-box">Result</label>
+                <div className={styles["button-container"]}>
+                    <input id="result-box" type="checkbox" className={styles["result-button"]} checked={result} onChange={e => setResult(!result)}></input>
+                </div>
+            </div>
+            <div>
+                <label htmlFor="date-box">Date</label>
+                <input id="date-box" type="date" value={date} onChange={e => setDate(e.target.value)}></input>
+            </div>
+            <div>
+                <label htmlFor="comments-box">Comments</label>
+                <input id="comments-box" type="text" value={comments} onChange={e => setComments(e.target.value)}></input>
+            </div>
+            <div className="bottom-modal-buttons">
+                <button type="submit">Submit</button>
+                <button onClick={() => setViewModal(false)}>Cancel</button>
+            </div>
+        </form>
+    )
+}
+
+export default AddGameForm
