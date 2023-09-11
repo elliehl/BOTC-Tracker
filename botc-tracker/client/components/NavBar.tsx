eb2ac7706680c0e3e4f46b61d7c41d48dbaf5944
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-11 13:13:57 +0100

    Add link to player stats page

--- botc-tracker/client/components/NavBar.tsx
+++ botc-tracker/client/components/NavBar.tsx
@@ -6,7 +6,8 @@ const NavBar = () => {
     return (
         <div className={styles["navbar-container"]}>
             <div className={styles["navbar-left"]}>
-                <Link href="/GameData" className={styles["page-links"]}>Player Stats</Link>
+                <Link href="/GameData" className={styles["page-links"]}>Game History</Link>
+                <Link href="/PlayerStats" className={styles["page-links"]}>Player Stats</Link>
             </div>
             <div className={styles["navbar-right"]}>
                 <Link href="/about" className={styles["page-links"]}>About</Link>
