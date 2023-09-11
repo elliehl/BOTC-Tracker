db58c7ad4deffc02692c39b97537d0506c6f304c
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-11 13:13:29 +0100

    Remove stats to put on separate page

--- botc-tracker/client/pages/GameData.tsx
+++ botc-tracker/client/pages/GameData.tsx
@@ -1,23 +1,13 @@
 import React from "react"
 import styles from '../styles/gameData.module.css'
-// import AlignmentStats from "../components/GameData/AlignmentStats"
-// import CharacterTypeStats from "../components/GameData/CharacterTypeStats"
 import DisplayGames from "../components/GameData/GameHistory"
-// import RoleStats from "../components/GameData/RoleStats"
 import "../styles/AlignmentStats.module.css"
-// import AddGameButton from "../components/GameData/AddGameButton"
-// import RoleStatsTest from "../components/GameData/RoleStats"
 
 const GameData = () => {
 
     return (
         <>
-        {/* <AddGameButton /> */}
-        <div className={styles["main-page"]}>
-            {/* <AlignmentStats /> */}
-            {/* <CharacterTypeStats /> */}
-            {/* <RoleStatsTest /> */}
-        </div>
+
         <div className={styles["main-page"]}>
         <DisplayGames />
         </div>
