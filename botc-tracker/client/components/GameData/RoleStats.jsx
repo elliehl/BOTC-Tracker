41fd8bb215389d78fb01643d8b4a5877f8b54989
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-18 20:00:49 +0100

    Add ability to sort by all values

--- botc-tracker/client/components/GameData/RoleStats.jsx
+++ botc-tracker/client/components/GameData/RoleStats.jsx
@@ -3,27 +3,60 @@ import styles from "../../styles/RoleStats.module.css"
 
 const RoleStats = () => {
     const [roleStats, setRoleStats] = useState([])
+    const [sortedRoleStats, setSortedRoleStats] = useState([])
 
     const getRoleStats = async () => {
         const res = await fetch('https://localhost:7240/api/Stats/roleStats')
         res.json().then((res) => setRoleStats(res))
     }
 
-    useEffect(() => {
-        getRoleStats()
-    }, [])
+    const winPercentageCalculator = () => {
+        for (let i = 0; i < roleStats.length; i++) {
+           roleStats[i].winPercentage = (Math.round((roleStats[i].wins / roleStats[i].games) * 100))
+        }
+    }
 
     const findTopFiveMostGamesPlayed = () => {
-        let sortRanking = roleStats.sort((a, b) => b.games - a.games)
-        console.log(sortRanking, 'sort ranking')
+        let sortRanking = [...roleStats].sort((a, b) => b.games - a.games)
         let topFive = sortRanking.slice(0,5)
         return topFive
     } 
 
-    let topData = findTopFiveMostGamesPlayed(roleStats)
+    const findTopFiveMostWins = () => {
+        let sortRanking = [...roleStats].sort((a, b) => b.wins - a.wins || b.games - a.games)
+        let topFive = sortRanking.slice(0,5)
+        return topFive
+    }
+
+    const findTopFiveHighestWinPercentage = () => {
+        winPercentageCalculator();
+        let sortRanking = [...roleStats].sort((a, b) => b.winPercentage - a.winPercentage || b.games - a.games)
+        let topFive = sortRanking.slice(0,5)
+        return topFive
+    }
+
+    let topGames = findTopFiveMostGamesPlayed(roleStats)
+    let topWins = findTopFiveMostWins(roleStats)
+    let topWinPercentage = findTopFiveHighestWinPercentage(roleStats)
+
+    const onSelectChange = (e) => {
+        e.target.value === 'games' ? setSortedRoleStats(topGames) : 
+        e.target.value === 'wins' ? setSortedRoleStats(topWins) : setSortedRoleStats(topWinPercentage)
+    }
+
+    useEffect(() => {
+        getRoleStats();
+        winPercentageCalculator();
+    }, [])
 
     return (
-        <table className={styles['role-stats-container']}>
+        <div className={styles['role-stats-container']}>
+        <select defaultValue={'games'} onChange={onSelectChange} className={styles['select-button']}>
+            <option value={'games'}>Games</option>
+            <option value={'wins'}>Wins</option>
+            <option value={'winPercentage'}>Win %</option>
+        </select>
+        <table>
             <thead>
                 <tr>
                     <th>Role</th>
@@ -32,20 +65,33 @@ const RoleStats = () => {
                     <th>Win %</th>
                 </tr>
             </thead>
-            {topData.map((rank) => {
+            {sortedRoleStats.length !== 0 ? sortedRoleStats.map((rank) => {
                 return (
                     <tbody>
                         <tr>
                             <td>{rank.starting_Role}</td>
                             <td>{rank.games}</td>
                             <td>{rank.wins}</td>
-                            <td>{Math.round((rank.wins / rank.games) * 100)}%</td>
+                            <td>{rank.winPercentage}%</td>
+                        </tr>
+                    </tbody>
+                )
+            })
+            : topGames.map((rank) => {
+                return (
+                    <tbody>
+                        <tr>
+                            <td>{rank.starting_Role}</td>
+                            <td>{rank.games}</td>
+                            <td>{rank.wins}</td>
+                            <td>{rank.winPercentage}%</td>
                         </tr>
                     </tbody>
                 )
             })
             }
         </table>
+        </div>
     )
 }
 
