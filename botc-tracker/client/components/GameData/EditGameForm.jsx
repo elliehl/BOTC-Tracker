c9d4d729dc0e994e36fb13d6f3ac2c96e821579f
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-19 18:13:12 +0100

    Fix so date now shows after clicking edit

--- botc-tracker/client/components/GameData/EditGameForm.jsx
+++ botc-tracker/client/components/GameData/EditGameForm.jsx
@@ -2,6 +2,10 @@ import { GameContext } from "../../contexts/GameContext"
 import styles from "../../styles/AddGameButton.module.css"
 import { useContext, useEffect, useState } from "react"
 import roleDataArray from "./roleData";
+import { Abel } from '@next/font/google'
+
+const abel = Abel({ weight: '400', subsets: ['latin'] })
+
 const roleList = roleDataArray.map((role) => role.name).sort()
 
 const EditGameForm = ({selectedGame}) => {
@@ -13,7 +17,7 @@ const EditGameForm = ({selectedGame}) => {
     const [game_Won, setGame_Won] = useState(selectedGame.game_Won)
     const [starting_Role, setStarting_Role] = useState(selectedGame.starting_Role)
     const [final_Role, setFinal_Role] = useState(selectedGame.final_Role)
-    const [date, setDate] = useState(selectedGame.date)
+    const [date, setDate] = useState(new Date(selectedGame.date).toISOString().split('T')[0])
     const [comments, setComments] = useState(selectedGame.comments)
 
     const {updateGame} = useContext(GameContext)
@@ -26,16 +30,17 @@ const EditGameForm = ({selectedGame}) => {
     }
 
     return (
-        <form action="/GameData" method="PUT" id="overallForm" className={styles["form"]} onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="starting-role-box">Starting Role</label>
+        <div className={abel.className}>
+        <form action="/GameData" method="PUT" id="overallForm" className={styles["edit-form"]} onSubmit={handleSubmit}>
+            <div className={styles["form-row"]}>
+                <label htmlFor="starting-role-box" className={styles["left-label"]}>Starting Role</label>
                 <input id="starting-role-box" list="roles" name="Starting Role" required={true} value={starting_Role} onChange={(e) => setStarting_Role(e.target.value)}></input>
                 <datalist id="roles">
                     {roleList.map((roleName) => <option>{roleName}</option>)}
                 </datalist>
             </div>
-            <div>
-                <label htmlFor="final-role-box">Final Role</label>
+            <div className={styles["form-row"]}>
+                <label htmlFor="final-role-box" className={styles["left-label"]}>Final Role</label>
                 <input id="final-role-box" list="roles" name="Final Role" required={true} value={final_Role} onChange={(e) => setFinal_Role(e.target.value)}></input>
                 <datalist id="roles">
                     {roleList.map((roleName) => <option>{roleName}</option>)}
@@ -52,19 +57,20 @@ const EditGameForm = ({selectedGame}) => {
                     <input id="result-box" type="checkbox" className={styles["result-button"]} checked={game_Won} onChange={e => setGame_Won(!game_Won)}></input>
                 </div>
             </div>
-            <div>
-                <label htmlFor="date-box">Date</label>
+            <div className={styles["form-row"]}>
+                <label htmlFor="date-box" className={styles["left-label"]}>Date</label>
                 <input id="date-box" type="date" value={date} onChange={e => setDate(e.target.value)}></input>
             </div>
-            <div>
-                <label htmlFor="comments-box">Comments</label>
+            <div className={styles["form-row"]}>
+                <label htmlFor="comments-box" className={styles["left-label"]}>Comments</label>
                 <input id="comments-box" type="text" value={comments} onChange={e => setComments(e.target.value)}></input>
             </div>
-            <div className="bottom-modal-buttons">
+            <div className={styles["modal-submit-button"]}>
                 <button type="submit">Submit</button>
                 <button onClick={() => setViewModal(false)}>Cancel</button>
             </div>
         </form>
+        </div>
     )
 }
 
