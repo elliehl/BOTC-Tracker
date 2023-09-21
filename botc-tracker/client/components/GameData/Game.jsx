f48acd9488dfd5132ccb2a24e3a4fa629a155f7b
Ellie <101657425+elliehl@users.noreply.github.com>
2023-09-21 22:54:18 +0100

    Fix edit styling and date display

--- botc-tracker/client/components/GameData/Game.jsx
+++ botc-tracker/client/components/GameData/Game.jsx
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from "react"
 import { GameContext } from "../../contexts/GameContext"
 const Modal = require("react-modal")
-import "../../styles/AddGameButton.module.css"
 import EditGameForm from "./EditGameForm"
+import styles from "../../styles/AddGameButton.module.css"
 
 
 const Game = ({game}) => {
@@ -23,12 +23,13 @@ const Game = ({game}) => {
             <td>{game.is_Evil === false ? 'Good' : 'Evil'}</td>
             <td>{game.game_Won === false ? 'Loss' : 'Win'}</td>
             <td>{game.comments}</td>
-            <td>{game.date == "0001-01-01T00:00:00" ? 'No Date' : new Date(game.date).toDateString()}</td>
+            <td>{game.date == "0001-01-01T00:00:00" || game.date == "" ? 'No Date' : new Date(game.date).toDateString()}</td>
             <td><button onClick={() => setViewEditModal(true)} type="button">Edit</button></td>
             <td><button onClick={() => deleteGame(game.id)} type="button">Delete</button></td>
 
-            <Modal isOpen={viewEditModal} onRequestClose={() => setViewEditModal(false)} className="modal">
+            <Modal isOpen={viewEditModal} onRequestClose={() => setViewEditModal(false)} className={styles["modal"]}>
                 <EditGameForm selectedGame={game}/>
+                {console.log(game)}
             </Modal>
         </>
     )
