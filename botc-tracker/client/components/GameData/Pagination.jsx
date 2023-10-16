bebe242f38c904974841b618ea01b9d64c9ea8ed
Ellie <101657425+elliehl@users.noreply.github.com>
2023-10-16 13:55:15 +0100

    Add functional pagination with dynamic page count

--- botc-tracker/client/components/GameData/Pagination.jsx
+++ botc-tracker/client/components/GameData/Pagination.jsx
@@ -2,22 +2,29 @@ import styles from "../../styles/Pagination.module.css"
 import { useContext } from "react"
 import { GameContext } from "../../contexts/GameContext"
 
-
-
-const Pagination = () => {
+const Pagination = ({gamesPerPage, setCurrentPage, firstGameOnPage, lastGameOnPage, currentPage}) => {
     const {gameHistory} = useContext(GameContext)
+    
+    const pages = []
+
+    for (let i = 1; i <= Math.ceil(gameHistory.length / gamesPerPage); i++) {
+            pages.push(i)
+        }
+
+    const isLastPage = (lastGameOnPage > gameHistory.length)
 
     return (
         <div className={styles["page-container"]}>
-            <div>Displaying x of {gameHistory.length} total games</div>
+            {isLastPage ? 
+            <div>Displaying {firstGameOnPage + 1}-{gameHistory.length} of {gameHistory.length} total games</div>
+            : <div>Displaying {firstGameOnPage + 1}-{lastGameOnPage} of {gameHistory.length} total games</div>
+            }
             <ul className={styles["page-list-container"]}>
-                <li className={styles["page-item"]}><a href="#">Previous</a></li>
-                <li className={styles["page-item"]}><a href="#">1</a></li>
-                <li className={styles["page-item"]}><a href="#">2</a></li>
-                <li className={styles["page-item"]}><a href="#">3</a></li>
-                <li className={styles["page-item"]}><a href="#">4</a></li>
-                <li className={styles["page-item"]}><a href="#">5</a></li>
-                <li className={styles["page-item"]}><a href="#">Next</a></li>
+                <li className={styles[`${currentPage === 1 ? 'page-item-disabled' : 'page-item'}`]}><a href="#" onClick={() => setCurrentPage((prev) => prev === 1 ? prev : prev - 1)}>Previous</a></li>
+                {pages.map((pageNumber) => {
+                    return <li className={styles[`${pageNumber === currentPage ? 'page-item-current' : 'page-item'}`]}><a href="#" onClick={() => setCurrentPage(pageNumber)}>{pageNumber}</a></li>
+                })}
+                <li className={styles[`${isLastPage ? 'page-item-disabled' : 'page-item'}`]}><a href="#" onClick={() => setCurrentPage(isLastPage ? currentPage : currentPage + 1)}>Next</a></li>
             </ul>
         </div>
     )
