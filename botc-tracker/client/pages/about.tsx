5ecc5f6803e37d411b9c9d9509a7132267eb86fd
Ellie <101657425+elliehl@users.noreply.github.com>
2023-10-18 17:24:59 +0100

    Add some more FAQs to about page

--- botc-tracker/client/pages/about.tsx
+++ botc-tracker/client/pages/about.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import styles from '../styles/About.module.css';
 
 const About = () => {
     return (
-        <div>
-        <p>This tracker only supports official roles from the game.</p>
-        <p>Lil' Monsta isn't included as a playable character since the starting role is a minion, so with the way I decided to display the data it would be impossible to play as Lil' Monsta.</p>
+        <div className={styles['faq-container']}>
+            <br/>
+            <div>
+            <p>Can I track homebrew characters?</p>
+            <p>This app only supports characters from the official game.</p>
+            </div>
+            <br/>
+            <div>
+            <p>Why is Lil' Monsta not in the character list?</p>
+            <p>As players cannot be dealt the Lil' Monsta token, a player cannot technically <em>play as</em> Lil' Monsta.</p>
+            </div>
+            <br/>
+            <div>
+            <p>Can I add track other parts of my games, such as script or storyteller?</p>
+            <p>This is not possible at this current time.</p>
+            </div>
+            <br/>
+            <div>
+            <p>How do I provide feedback?</p>
+            <p>Feel free to send any feedback and suggestions to <a href="https://github.com/elliehl"><strong>elliehl</strong></a> on Github.</p>
+            </div>
+            <br/>
         </div>
     )
 }
