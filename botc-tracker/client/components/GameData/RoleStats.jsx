cc7fdd2026f1888b0ddfb6b48d42a437d2519170
Ellie <101657425+elliehl@users.noreply.github.com>
2023-10-27 18:00:35 +0100

    Add high-low sorting and remove stats for roles with 0 games

--- botc-tracker/client/components/GameData/RoleStats.jsx
+++ botc-tracker/client/components/GameData/RoleStats.jsx
@@ -4,10 +4,12 @@ import styles from "../../styles/RoleStats.module.css"
 const RoleStats = () => {
     const [roleStats, setRoleStats] = useState([])
     const [sortedRoleStats, setSortedRoleStats] = useState([])
+    const [isHighToLow, setIsHighToLow] = useState(true)
+    const [currentDisplay, setCurrentDisplay] = useState('games')
 
     const getRoleStats = async () => {
         const res = await fetch('https://localhost:7240/api/Stats/roleStats')
-        res.json().then((res) => setRoleStats(res))
+        res.json().then((res) => setRoleStats(res.filter((stats) => stats.games !== 0)))
     }
 
     const winPercentageCalculator = () => {
@@ -17,20 +19,23 @@ const RoleStats = () => {
     }
 
     const findTopFiveMostGamesPlayed = () => {
-        let sortRanking = [...roleStats].sort((a, b) => b.games - a.games)
+        winPercentageCalculator();
+        let sortRanking = isHighToLow ? [...roleStats].sort((a, b) => b.games - a.games || b.wins - a.wins) :
+                                        [...roleStats].sort((a, b) => a.games - b.games || b.wins - a.wins)
         let topFive = sortRanking.slice(0,5)
         return topFive
     } 
 
     const findTopFiveMostWins = () => {
-        let sortRanking = [...roleStats].sort((a, b) => b.wins - a.wins || b.games - a.games)
+        let sortRanking = isHighToLow ? [...roleStats].sort((a, b) => b.wins - a.wins || a.games - b.games) :
+                                        [...roleStats].sort((a, b) => a.wins - b.wins || b.games - a.games)  
         let topFive = sortRanking.slice(0,5)
         return topFive
     }
 
     const findTopFiveHighestWinPercentage = () => {
-        winPercentageCalculator();
-        let sortRanking = [...roleStats].sort((a, b) => b.winPercentage - a.winPercentage || b.games - a.games)
+        let sortRanking = isHighToLow ? [...roleStats].sort((a, b) => b.winPercentage - a.winPercentage || b.games - a.games) :
+                                        [...roleStats].sort((a, b) => a.winPercentage - b.winPercentage || b.games - a.games)
         let topFive = sortRanking.slice(0,5)
         return topFive
     }
@@ -39,25 +44,62 @@ const RoleStats = () => {
     let topWins = findTopFiveMostWins(roleStats)
     let topWinPercentage = findTopFiveHighestWinPercentage(roleStats)
 
+    const selectChangeGames = () => {
+        setSortedRoleStats(topGames)
+        setCurrentDisplay('games')
+    }
+
+    const selectChangeWins = () => {
+        setSortedRoleStats(topWins)
+        setCurrentDisplay('wins')
+    }
+
+    const selectChangeWinPercentage = () => {
+        setSortedRoleStats(topWinPercentage)
+        setCurrentDisplay('winPercentage')
+    }
+
     const onSelectChange = (e) => {
-        e.target.value === 'games' ? setSortedRoleStats(topGames) : 
-        e.target.value === 'wins' ? setSortedRoleStats(topWins) : setSortedRoleStats(topWinPercentage)
+        e.target.value === 'games' ? selectChangeGames() : 
+        e.target.value === 'wins' ? selectChangeWins() : selectChangeWinPercentage()
+    }
+
+    const onSelectChangeOrdering = (e) => {
+        e.target.value === 'highest' ? setIsHighToLow(true) : setIsHighToLow(false)
     }
 
     useEffect(() => {
         getRoleStats();
-        winPercentageCalculator();
     }, [])
+
+    useEffect(() => {
+        if(currentDisplay === 'wins') {
+            setSortedRoleStats(topWins)
+        } else if (currentDisplay === 'games') {
+            setSortedRoleStats(topGames)
+        } else if (currentDisplay === 'winPercentage') {
+            setSortedRoleStats(topWinPercentage)
+        }
+    }, [isHighToLow])
+
 
     return (
         <div className={styles['role-stats-container']}>
         <div className={styles["sort-row"]}>
-        <h4 className={styles["sort-heading"]}>Sort by: </h4>
-        <select defaultValue={'games'} onChange={onSelectChange} className={styles['select-button']}>
-            <option value={'games'}>Games</option>
-            <option value={'wins'}>Wins</option>
-            <option value={'winPercentage'}>Win %</option>
-        </select>
+            <div>
+                <h4 className={styles["sort-heading"]}>Sort by: </h4>
+            </div>
+            <div className={styles["sort-row-right"]}>
+                <select defaultValue={'games'} onChange={onSelectChange} className={styles['select-button']}>
+                    <option value={'games'}>Games</option>
+                    <option value={'wins'}>Wins</option>
+                    <option value={'winPercentage'}>Win %</option>
+                </select>
+                <select defaultValue={'highest'} onChange={onSelectChangeOrdering} className={styles['select-button']}>
+                    <option value={'highest'}>Highest</option>
+                    <option value={'lowest'}>Lowest</option>
+                </select>
+            </div>
         </div>
         <table>
             <thead>
