e450ce1c3b6bef7d7e7986ebd360aa7490d11d13
Ellie <101657425+elliehl@users.noreply.github.com>
2023-10-30 13:23:10 +0000

    Add logged in check

--- botc-tracker/client/pages/PlayerStats.jsx
+++ botc-tracker/client/pages/PlayerStats.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useContext } from "react"
+import {GameContext} from "../contexts/GameContext"
 import styles from '../styles/gameData.module.css'
 import OverallStats from "../components/GameData/OverallStats"
 import RoleStats from "../components/GameData/RoleStats"
@@ -6,7 +7,12 @@ import AlignmentStats from "../components/GameData/AlignmentStats"
 import CharacterTypeStats from "../components/GameData/CharacterTypeStats"
 
 const PlayerStats = () => {
+
+    const { isLoggedIn } = useContext(GameContext)
+
     return (
+        <div>
+        {isLoggedIn ? (
         <div className={styles["stats-page"]}>
             <RoleStats />
             <div className={styles["stats-page-right"]}>
@@ -14,6 +20,9 @@ const PlayerStats = () => {
                 <AlignmentStats />
                 <CharacterTypeStats />
              </div>
+        </div>
+        ) : <h3>Please log in</h3>
+        }
         </div>
     )
 }
