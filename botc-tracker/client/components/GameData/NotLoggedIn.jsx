9820c283458477bd39d14e85efd2d8a920b253ad
Ellie <101657425+elliehl@users.noreply.github.com>
2023-11-01 15:24:56 +0000

    Add sign up modal to not logged in component

--- botc-tracker/client/components/GameData/NotLoggedIn.jsx
+++ botc-tracker/client/components/GameData/NotLoggedIn.jsx
@@ -1,11 +1,22 @@
+import { useContext } from "react"
 import styles from "../../styles/NotLoggedIn.module.css"
+import { GameContext } from "../../contexts/GameContext"
+const Modal = require("react-modal")
+import CreateAccountModal from "./CreateAccountModal"
 
 const NotLoggedIn = () => {
+
+    const { viewCreateAccountModal, setViewCreateAccountModal } = useContext(GameContext)
+
     return (
         <div className={styles["container"]}>
             <h2>You are not currently logged in</h2>
             <p><a href="/Login">Log in</a> to view your stats</p>
-            <p>Or sign up to get started!</p>
+            <p>Or <button className={styles["sign-up-button"]} onClick={() => setViewCreateAccountModal(true)}>sign up </button> to get started!</p>
+
+            <Modal isOpen={viewCreateAccountModal} onRequestClose={() => setViewCreateAccountModal(false)} className={styles["modal"]}>
+                <CreateAccountModal setViewCreateAccountModal={setViewCreateAccountModal}/>
+            </Modal>
         </div>
     )
 }
