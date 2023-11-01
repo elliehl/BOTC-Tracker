eb3d338e2c763b355e2a442fe4c3bd54ff5bebc4
Ellie <101657425+elliehl@users.noreply.github.com>
2023-11-01 15:24:30 +0000

    Update format for styling

--- botc-tracker/client/components/GameData/CreateAccountModal.jsx
+++ botc-tracker/client/components/GameData/CreateAccountModal.jsx
@@ -1,22 +1,32 @@
-import styles from "../../styles/AddGameButton.module.css"
+import { useContext } from "react"
+import styles from "../../styles/Login.module.css"
+import { GameContext } from "../../contexts/GameContext"
 
 const CreateAccountModal = ({setViewCreateAccountModal}) => {
+    
+    const { abel } = useContext(GameContext)
 
     return (
-        <div className={styles["form"]}>
-            <div>
-                <h2>Sign Up</h2>
-                <button>X</button>
-            </div>
-            <div>
-                <input type="text" placeholder="Username"></input>
-            </div>
-            <div>
-                <input type="text" placeholder="Password"></input>
-            </div>
-            <div>
-                <button type="submit">Create account</button>
-                <button onClick={() => setViewCreateAccountModal(false)}>Cancel</button>
+        <div className={abel.className}>
+            <div className={styles['container']}>
+                <form className={styles['form']}>
+                    <div className={styles["signup-close-icon"]}>
+                        <button onClick={() => setViewCreateAccountModal(false)}>X</button>
+                    </div>
+                    <div className={styles["signup-top-row"]}>
+                        <h2>Sign Up</h2>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Username"></input>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Password"></input>
+                    </div>
+                    <div className={styles["signup-bottom-buttons"]}>
+                        <button type="submit">Create account</button>
+                        <button onClick={() => setViewCreateAccountModal(false)}>Cancel</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
